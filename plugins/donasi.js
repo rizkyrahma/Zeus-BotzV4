cf7dcc80dbcddec03e4621b747206d9616df0a96
The.sad.boy01 <88497118+Kangsad01@users.noreply.github.com>
2022-02-12 18:42:21 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,5 @@
 let fetch = require('node-fetch')
-let sawer = global.donate
-let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
-${sawer}
-`.trim(), 'Yang donasi dapat pahala', 'Owner', '.owner', m)
+let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), donate, 'Yang donasi dapat pahala', 'Owner', '.owner', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
