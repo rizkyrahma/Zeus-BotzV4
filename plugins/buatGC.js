b475dbc5b4e59a9a17b95e51a791f86675e14c4b
The.sad.boy01 <88497118+Kangsad01@users.noreply.github.com>
2022-02-13 10:07:58 +0700

    Update buatGC.js

--- plugins/buatGC.js
+++ plugins/buatGC.js
@@ -10,7 +10,7 @@ let handler = async(m, { conn, text }) => {
     let url = 'https://chat.whatsapp.com/' + link;
     console.log(chalk.bold.red('Membuat Grup: ' + group.gid + '\nNama Grup: ' + text + '\n\nViolet'))
     //conn.sendMessage(group.gid, "Success to group create!", MessageType.extendedText)
-     m.reply('_Berhasil Membuat Grup *' + text + '*_\n\n*Nama:* ' + text + '\n*ID:* ' + group.gid + '\n*Link:* ' + url + '\n\n*@ẉa.me/ᵀᵒʰʳᵘ-ᴮᴼᵀᶻ*')
+     m.reply('_Berhasil Membuat Grup *' + text + '*_\n\n*Nama:* ' + text + '\n*ID:* ' + group.gid + '\n*Link:* ' + url + '\n\n' + wm)
        } catch (e) {
     m.reply('```Error```')
     console.log (e)
@@ -20,17 +20,6 @@ handler.help = ['creategroup']
 handler.tags = ['owner']
 handler.command = /^((create|buat)(gc|grup|group))$/
 handler.owner = true
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.register = true
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.limit = false
 
 module.exports = handler
 ///////////////////////
