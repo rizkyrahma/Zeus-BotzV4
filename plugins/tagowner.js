2977d04606cc3ab0e217bd1e31e8cae5724e9ec7
Zeus Botz <97302454+PilarV2@users.noreply.github.com>
2022-02-17 14:28:24 +0700

    Add files via upload

--- plugins/tagowner.js
+++ plugins/tagowner.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
-conn.reply(global.owner[0] + `@s.whatsapp.net`, ` *@62895336282144 Ada yang memanggil anda :V*`, m)
+conn.reply(global.owner[0] + `@s.whatsapp.net`, ` *@6289625556161 Ada yang memanggil anda :V*`, m)
 
   conn.reply(m.chat, `
-Kak ᴛʜᴇ.ꜱᴀᴅ.ʙᴏʏ01⁩ (@62895336282144  ), Ada Yang Manggil anda😊
+Kak Pilar⁩ (@6289625556161  ), Ada Yang Manggil anda😊
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /@62895336282144 /i
+handler.customPrefix = /@6289625556161 /i
 handler.command = new RegExp
 
 module.exports = handler
