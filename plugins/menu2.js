8eeccc65732a0409955f3c834ef9761dc3c92c05
Zeus Botz <97302454+PilarV2@users.noreply.github.com>
2022-02-22 10:35:25 +0700

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -129,20 +129,20 @@ let handler  = async (m, { conn, usedPrefix: _p, DevMode }) => {
 let fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${nama}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
 
   sumberImg = fs.readFileSync(`./src/logo.jpg`)
-  image = (await conn.prepareMessage('6288217277973@s.whatsapp.net', sumberImg, MessageType.image, { thumbnail: Buffer.alloc(0) })).message.imageMessage
+  image = (await conn.prepareMessage('62896255561617@s.whatsapp.net', sumberImg, MessageType.image, { thumbnail: Buffer.alloc(0) })).message.imageMessage
   res = await conn.prepareMessageFromContent(m.chat, {
     "productMessage": {
       "product": {
         "productImage": image,
         "productId": "4938174216214248",
         "title": `${ucapan()}, ${conn.getName(m.sender)} !`,
-        "description": '◈ Made by The.sad.boy01\n\n' + text,
+        "description": '◈ Recode By Pilar\n\n' + text,
         "retailerId":  `${week} ${date}\n\nTime: ${time} Wib`,
-        "url": 'github.com/Kangsad01',
+        "url": 'github.com/PilarV2',
         "descriptionCount": "999999999",
         "productImageCount": "1",
       },
-      "businessOwnerJid": "62895336282144@s.whatsapp.net",
+      "businessOwnerJid": "62896255561617@s.whatsapp.net",
       "contextInfo": {
         "forwardingScore": 9999,
         "isForwarded": true
