5bdbcb432fef23b54fa2209b48ad9ec772ddfc16
Nur Alam <99713260+rizkyrahma@users.noreply.github.com>
2022-03-08 21:47:59 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -14,7 +14,7 @@ handler.all = async function (m, { isBlocked }) {
     try {
         if (m.mentionedJid.includes(this.user.jid) && m.isGroup) {
             await this.send2Button(m.chat,
-                isBanned ? 'ZeusBotz tidak aktif' : banned ? 'kamu dibanned' : 'ZeusBotz disini',
+                isBanned ? 'AlamBotz tidak aktif' : banned ? 'kamu dibanned' : 'AlamBotz disini',
                 wm,
                 isBanned ? 'Unban' : banned ? 'Pemilik Bot' : 'Menu',
                 isBanned ? '.unban' : banned ? '.owner' : '.menu',
@@ -56,7 +56,7 @@ handler.all = async function (m, { isBlocked }) {
         let _uptime = process.uptime() * 1000
         let uptime = clockString(_uptime)
         totaluser = Object.keys(db.data.users)
-        await this.setStatus(`Aktif selama ${uptime} │ Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} │ total user : ${totaluser.length} │ ZeusBotz`).catch(_ => _)
+        await this.setStatus(`Aktif selama ${uptime} │ Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} │ total user : ${totaluser.length} │ AlamBotz`).catch(_ => _)
         setting.status = new Date() * 1
     }
 
